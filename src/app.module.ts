2b6aeaa8a120afd109a298491b93fd712dd99766
tazska <leyplay14@gmail.com>
2026-03-18 19:33:27 -0500

    Base

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { DepartamentoModule } from './departamento/departamento.module';
+import { CargoModule } from './cargo/cargo.module';
+import { EmpleadoModule } from './empleado/empleado.module';
+import { ProyectoModule } from './proyecto/proyecto.module';
+import { TareaModule } from './tarea/tarea.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    DepartamentoModule,
+    CargoModule,
+    EmpleadoModule,
+    ProyectoModule,
+    TareaModule,
+  ],
+})
+export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+// src/database/database.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
